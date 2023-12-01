92d0dc32d5ba6789ffc57d66cd5e8e6d4f7a26cf
ely896 <saretta2011@libero.it>
2023-12-01 12:24:06 +0100

    Add Mail

--- Mail/script.js
+++ Mail/script.js
@@ -1,3 +1,33 @@
 /*Mail
 Chiedi all’utente la sua email, controlla che sia nella lista di chi può 
 accedere, stampa un messaggio appropriato sull’esito del controllo*/
+
+console.log('funziona');
+
+//prompt per chiedere l'email
+
+//array con lista invitati
+
+//confronto tra elementi dell'array e variabile
+
+//Dichiarazione variabile richiesta email
+
+const userEmail = prompt('inserisci la tua email');
+
+//Dichiarazione array lista invitati
+
+let guestList = ["luca@gmail.com", "federica@libero.it", "giovanni@gmail.com", "agostin@gmail.com",];
+
+
+for (let i = 0; i < guestList.length; i++) {
+
+   console.log(guestList[i]);
+    
+    if (userEmail=== guestList [i]) {
+        console.log('puio entrare');
+    } else {
+        console.log('non puoi entrare');
+    }
+};
+
+
